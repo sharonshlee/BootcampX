0454c2c3317f4d0fe3b8fa892ac08a9410d5c5a6
Sharon Lee <sharonshlee@icloud.com>
2021-09-14 07:52:50 -0400

    add new tables and data

--- 2_queries_joins/cohort_total_duration.sql
+++ 2_queries_joins/cohort_total_duration.sql
@@ -1,4 +1,4 @@
-SELECT SUM(duration) as total_duration
+SELECT SUM(a_s.duration) as total_duration
 FROM assignment_submissions a_s
 JOIN students s on a_s.student_id = s.id
 JOIN cohorts c on s.cohort_id = c.id
